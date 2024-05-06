fe70702a48bb2d13e95fc32be249da796fe65930
arpipatel-ap <arpi96@gmail.com>
2024-05-06 19:21:58 -0400

    Installing Mocha and Chai

--- test/headtest.js
+++ test/headtest.js
@@ -1,11 +1,28 @@
-const head = require("../head");
-//const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+const head   = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
 
-head([5, 6, 7]), 5; 
-head(["Hello", "Lighthouse", "Labs"]), "Hello"; 
-head([]), undefined; 
+it("returns 5 for [5, 6, 7]", () => {
+  assert.strictEqual(head([5, 6, 7]), 5);
+});
 
-// assertEqual(head([5, 6, 7]), 5); 
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
-// assertEqual(head([]), undefined); 
+it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+  assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+});
+
+it("returns undefine for []", () => {
+  assert.strictEqual(head([]), undefined); 
+});
+
+// head([5, 6, 7]), 5; 
+// head(["Hello", "Lighthouse", "Labs"]), "Hello"; 
+// head([]), undefined; 
+
