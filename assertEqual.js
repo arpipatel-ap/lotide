7326b916cf200cd51bacad3b80f5deca356bc0a7
arpipatel-ap <arpi96@gmail.com>
2024-05-06 15:46:56 -0400

    Replace function with require

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1,1); 
-assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual(1,1); 
+// assertEqual("Lighthouse Labs", "Bootcamp");
 
+module.exports = assertEqual;
