d85e2e7e2e7994866562b6a0125dc152a6c54444
arpipatel-ap <arpi96@gmail.com>
2024-05-06 16:11:09 -0400

    Replace function with require

--- tail.js
+++ tail.js
@@ -1,11 +1,5 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function(array) {
   if (array.length <= 1) {
@@ -14,14 +8,16 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-// Test cases
-const result1 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result1.length, 2); 
-assertEqual(result1[0], "Lighthouse"); 
-assertEqual(result1[2], "Labs"); 
+// // Test cases
+// const result1 = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result1.length, 2); 
+// assertEqual(result1[0], "Lighthouse"); 
+// assertEqual(result1[2], "Labs"); 
 
-const result2 = tail([1]);
-assertEqual(result2.length, 0); 
+// const result2 = tail([1]);
+// assertEqual(result2.length, 0); 
 
-const result3 = tail([]);
-assertEqual(result3.length, 0); 
+// const result3 = tail([]);
+// assertEqual(result3.length, 0);
+
+module.exports = tail;
