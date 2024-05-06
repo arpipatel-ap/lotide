d8d4c44a4e748a22d6eb694d86b588dba45b75c0
arpipatel-ap <arpi96@gmail.com>
2024-05-06 15:47:52 -0400

    Add test file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("../assertEqual");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1,1); 
+assertEqual("Lighthouse Labs", "Bootcamp");
+
+// module.exports = assertEqual;
