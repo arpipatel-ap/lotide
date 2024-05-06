10e9bd4d9588af68991073258b60cea74e2eaafb
arpipatel-ap <arpi96@gmail.com>
2024-05-06 18:06:32 -0400

    Replace function with require

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
+
 const eqArrays = function(arr1, arr2) {
   
   if (arr1.length !== arr2.length) {
@@ -18,9 +13,5 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+ 
+module.exports = eqArrays;
