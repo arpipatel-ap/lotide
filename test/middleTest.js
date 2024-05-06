919b9aac3f031e7c641bb7d9726e11cae186d66c
arpipatel-ap <arpi96@gmail.com>
2024-05-06 18:29:40 -0400

    Add test file

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const middle = require("../middle");
+// const assertArraysEqual = require("../assertArraysEqual");
+
+console.log(middle([1,2]));
+console.log(middle([1,2,3]));
+console.log(middle([1,2,5,8,9,7,6,8]));
+console.log(middle([1,"Hello","world"]));
