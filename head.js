654cce4355c1386fc65ebc7f26cc0c75779c4401
arpipatel-ap <arpi96@gmail.com>
2024-05-06 15:48:36 -0400

    Replace function with require

--- head.js
+++ head.js
@@ -1,11 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const head = function(array) {
   if (array.length === 0) {
@@ -19,3 +21,4 @@ assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
 assertEqual(head([]), undefined); 
 
+module.exports = head;
