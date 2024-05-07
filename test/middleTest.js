37689bb8c01ae1deba394f51489b472872322eef
arpipatel-ap <arpi96@gmail.com>
2024-05-06 20:00:31 -0400

    Add Unit Testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,23 @@
+const { assert } = require("chai");
 const middle = require("../middle");
 // const assertArraysEqual = require("../assertArraysEqual");
 
-console.log(middle([1,2]));
-console.log(middle([1,2,3]));
-console.log(middle([1,2,5,8,9,7,6,8]));
-console.log(middle([1,"Hello","world"]));
+describe("#middle", () => {
+  it("retrun Empty array",() =>{
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns '8','9' for [1,2,5,8,9,7,6,8]", () => {
+    assert.deepEqual(middle([1,2,5,8,9,7,6,8]), [8 , 9]);
+  });
+  it("retrun 'Hello' for ['1','Hello', 'World']",() =>{
+    assert.deepEqual(middle([1, "Hello", "World"]), ['Hello']);
+  });
+});
+
+// console.log(middle([1,2]));
+// console.log(middle([1,2,3]));
+// console.log(middle([1,2,5,8,9,7,6,8]));
+// console.log(middle([1,"Hello","world"]));
