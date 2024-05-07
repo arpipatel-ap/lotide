a1a29c8f4fabd5dd6f4518a081f40495a11652d9
arpipatel-ap <arpi96@gmail.com>
2024-05-06 20:00:51 -0400

    Add Unit Testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,34 @@
+const assert = require('chai').assert;
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+//const assertEqual = require("../assertEqual");
 
-const result1 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result1.length, 2); 
-assertEqual(result1[0], "Lighthouse"); 
-assertEqual(result1[2], "Labs"); 
 
-const result2 = tail([1]);
-assertEqual(result2.length, 0); 
+describe("#tail", () => {
+  it("returns an array excluding the first element", () => {
+    const result1 = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result1.length, 2); 
+    assert.deepEqual(result1[0], "Lighthouse"); 
+    assert.deepEqual(result1[1], "Labs"); 
+  });
 
-const result3 = tail([]);
-assertEqual(result3.length, 0);
+  it("returns an array excluding for an element", () => {
+    const result2 = tail([1]);
+    assert.deepEqual(result2.length, 0); 
+  });
+  it("returns an array excluding for empty element", () => {
+    const result3 = tail([]);
+    assert.deepEqual(result3.length, 0); 
+  });
+
+});
+
+// const result1 = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result1.length, 2); 
+// assertEqual(result1[0], "Lighthouse"); 
+// assertEqual(result1[2], "Labs"); 
+
+// const result2 = tail([1]);
+// assertEqual(result2.length, 0); 
+
+// const result3 = tail([]);
+// assertEqual(result3.length, 0);
